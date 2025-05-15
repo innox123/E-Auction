c85983aba6b719c5ec9373cf51300450fb05744e
innox123 <innocentcyusa2@gmai.com>
2025-05-15 17:40:10 +0200

    Password rest

--- core/settings.py
+++ core/settings.py
@@ -206,3 +206,11 @@ CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "innocentcyusa2@gmail.com"
+EMAIL_HOST_PASSWORD = "qttv fzkf loyz zxez"
--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('auctions.urls')),
     path('account/', include('account.urls')),
     path('admin/dashboard/', include('admin_site.urls')),
 
+    path("resetpassword/", auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name="password_reset"),
+    path("resetpasswordsent/", auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_sent.html'), name="password_reset_done"),
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name="password_reset_confirm"),
+    path("resetpasswordcomplete/", auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name="password_reset_complete")
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
